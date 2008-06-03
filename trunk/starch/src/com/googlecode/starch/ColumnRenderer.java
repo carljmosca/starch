7b8fa88e64af719884cc40cb336ff7933da97f34
carljmosca <carljmosca@a3080d55-7d4e-0410-8870-6f489ad99ab0>
2008-06-03 13:52:16 +0000

    initial commit

---
+++ trunk/starch/src/com/googlecode/starch/ColumnRenderer.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.starch;
+
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ *
+ * @author moscac
+ */
+public class ColumnRenderer implements TableCellRenderer {
+
+    public ColumnRenderer() {
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int rowIndex, int vColIndex) {
+        // 'value' is value contained in the cell located at
+        // (rowIndex, vColIndex)
+
+        if (isSelected) {
+            // cell (and perhaps other cells) are selected
+        }
+
+        if (hasFocus) {
+            // this cell is the anchor and the table has the focus
+        }        
+
+        // Configure the component with the specified value
+        if (value instanceof Integer) {
+            label.setText(getDisplayValue((Integer)value));
+        }
+
+        // Set tool tip if desired
+        //label.setToolTipText((String) value);
+
+        // Since the renderer is a component, return itself
+        return label;
+    }
+    
+    private String getDisplayValue(int lookupValue) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getLookupValue() == lookupValue) {
+                return items.get(i).getDisplayValue();
+            }
+        }
+        return "";
+    }
+    
+    public void addLookupItem(int lookupValue, String displayValue) {
+        items.add(new ColumnLookupItem());
+        items.get(items.size() - 1).setLookupValue(lookupValue);
+        items.get(items.size() - 1).setDisplayValue(displayValue);
+    }
+    
+    private JLabel label = new JLabel();
+    private List<ColumnLookupItem> items = new ArrayList<ColumnLookupItem>(0);
+
+}
