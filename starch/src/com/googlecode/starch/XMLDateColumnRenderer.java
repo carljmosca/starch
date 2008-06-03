04de567ff72b2ce42f8221187391a0695d76150c
carljmosca <carljmosca@a3080d55-7d4e-0410-8870-6f489ad99ab0>
2008-06-03 20:13:42 +0000

    initial commit

---
+++ starch/src/com/googlecode/starch/XMLDateColumnRenderer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.starch;
+
+import com.googlecode.starch.util.DateConverter;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+/**
+ *
+ * @author moscac
+ */
+public class XMLDateColumnRenderer implements TableCellRenderer {
+
+    public XMLDateColumnRenderer() {
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int rowIndex, int vColIndex) {
+        // 'value' is value contained in the cell located at
+        // (rowIndex, vColIndex)
+
+        if (isSelected) {
+            // cell (and perhaps other cells) are selected
+        }
+
+        if (hasFocus) {
+            // this cell is the anchor and the table has the focus
+        }        
+
+        // Configure the component with the specified value
+        if (value instanceof XMLGregorianCalendar) {
+            label.setText(DateConverter.getDateAsString((XMLGregorianCalendar)value));
+        }
+
+        // Set tool tip if desired
+        //label.setToolTipText((String) value);
+
+        // Since the renderer is a component, return itself
+        return label;
+    }
+   
+    
+    private JLabel label = new JLabel();
+
+}
---
+++ starch/src/com/googlecode/starch/util/DateConverter.java
@@ -0,0 +1,158 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.starch.util;
+
+import java.sql.Time;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.logging.Logger;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+/**
+ *
+ * @author moscac
+ */
+public class DateConverter {
+    public DateConverter() {
+    }
+    /**
+     *
+     * @param date
+     * @return
+     */
+    private static Logger logger = Logger.getLogger(DateConverter.class.getName());
+
+    public static XMLGregorianCalendar dateToXML(Time time) {
+        Date date = new Date(time.getTime());
+        return dateToXML(time.getTime());
+    }
+
+    public static XMLGregorianCalendar dateToXML(Date date) {
+        return dateToXML(date.getTime());
+    }
+
+    public static XMLGregorianCalendar dateToXML(long time) {
+//        String sTimeZone = System.getProperty("user.timezone");
+//        if ((sTimeZone == null) || (sTimeZone.trim().length() == 0)) {
+//            sTimeZone = "America/New_York";
+//        }
+//        TimeZone tz = TimeZone.getTimeZone(sTimeZone);
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(time);
+        return dateToXML(calendar);
+    }
+
+    public static XMLGregorianCalendar dateToXML(String value) {
+        if (value.trim().length() != 8) {
+            return dateValueRepresentingNull();
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        try {
+            Date dValue = sdf.parse(value.trim(), new ParsePosition(0));
+            Integer year = new Integer(value.substring(0, 4));
+            Integer month = new Integer(value.substring(4, 6));
+            Integer day = new Integer(value.substring(6, 8));
+            GregorianCalendar cal = new GregorianCalendar();
+            cal.set(GregorianCalendar.YEAR, year.intValue());
+            cal.set(GregorianCalendar.MONTH, month.intValue() - 1);
+            cal.set(GregorianCalendar.DAY_OF_MONTH, day.intValue());
+            cal.set(GregorianCalendar.MINUTE, 1);
+            return dateToXML(cal);
+        } catch (NullPointerException npe) {
+            return dateValueRepresentingNull();
+        }
+    }
+
+    public static XMLGregorianCalendar dateToXML(GregorianCalendar calendar) {
+        javax.xml.datatype.XMLGregorianCalendar xCalendar = null;
+
+        try {
+            javax.xml.datatype.DatatypeFactory factory = javax.xml.datatype.DatatypeFactory.newInstance();
+            xCalendar = factory.newXMLGregorianCalendar(calendar);
+        } catch (DatatypeConfigurationException dte) {
+            logger.fine("Exception creating XMLGregorianCalendar: " + dte.getMessage());
+        }
+        return xCalendar;
+    }
+
+    public static Calendar calendarValueRepresentingNull() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, DATE_REPRESENTING_NULL_YEAR);
+        calendar.set(Calendar.MONTH, DATE_REPRESENTING_NULL_MONTH);
+        calendar.set(Calendar.DATE, DATE_REPRESENTING_NULL_DATE);
+        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.ZONE_OFFSET, (calendar.getTimeZone().getRawOffset() +
+                calendar.getTimeZone().getDSTSavings()) / 60000);
+        return calendar;
+    }
+
+    public static XMLGregorianCalendar dateValueRepresentingNull() {
+        XMLGregorianCalendar xCalendar = null;
+        try {
+            GregorianCalendar calendar = new GregorianCalendar();
+            calendar.set(GregorianCalendar.YEAR, DATE_REPRESENTING_NULL_YEAR);
+            calendar.set(GregorianCalendar.MONTH, DATE_REPRESENTING_NULL_MONTH);
+            calendar.set(GregorianCalendar.DATE, DATE_REPRESENTING_NULL_DATE);
+            calendar.set(GregorianCalendar.HOUR, 0);
+            calendar.set(GregorianCalendar.MINUTE, 0);
+            calendar.set(GregorianCalendar.SECOND, 0);
+            calendar.set(GregorianCalendar.MILLISECOND, 0);
+            calendar.set(GregorianCalendar.ZONE_OFFSET, (calendar.getTimeZone().getRawOffset() + calendar.getTimeZone().getDSTSavings()) / 60000);
+            javax.xml.datatype.DatatypeFactory factory = javax.xml.datatype.DatatypeFactory.newInstance();
+
+            xCalendar = factory.newXMLGregorianCalendar(calendar);
+        } catch (DatatypeConfigurationException dte) {
+            logger.fine("Exception creating XMLGregorianCalendar: " + dte.getMessage());
+        }
+        return xCalendar;
+    }
+
+    public static boolean isValueRepresentingNull(XMLGregorianCalendar calendar) {
+
+        return (calendar == null) ||
+                ((calendar.getYear() == DATE_REPRESENTING_NULL_YEAR) &&
+                //(calendar.getMonth() == DATE_REPRESENTING_NULL_MONTH) &&
+                (calendar.getDay() == DATE_REPRESENTING_NULL_DATE));
+    }
+
+    public static boolean isValueRepresentingNull(Calendar calendar) {
+
+        return (calendar == null) ||
+                ((calendar.get(Calendar.YEAR) == DATE_REPRESENTING_NULL_YEAR) &&
+                //(calendar.getMonth() == DATE_REPRESENTING_NULL_MONTH) &&
+                (calendar.get(Calendar.DAY_OF_MONTH) == DATE_REPRESENTING_NULL_DATE));
+    }
+
+    public static String getDateAsString(XMLGregorianCalendar value) {
+        if (isValueRepresentingNull(value)) {
+            return "";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        return sdf.format(value.toGregorianCalendar().getTimeInMillis());
+    }
+
+    public static TimeZone getTimeZone() {
+        String sTimeZone = System.getProperty("user.timezone");
+        if ((sTimeZone == null) || (sTimeZone.trim().length() == 0) || (sTimeZone.equalsIgnoreCase("GMT"))) {
+            sTimeZone = "America/New_York";
+        }
+        return TimeZone.getTimeZone(sTimeZone);
+    }
+    private static int DATE_REPRESENTING_NULL_YEAR = 1899;
+    private static int DATE_REPRESENTING_NULL_MONTH = Calendar.JANUARY;
+    private static int DATE_REPRESENTING_NULL_DATE = 1;
+    public static final long MILLISECS_PER_MINUTE = 60 * 1000;
+    public static final long MILLISECS_PER_HOUR = 60 * MILLISECS_PER_MINUTE;
+    public static final long MILLISECS_PER_DAY = 24 * MILLISECS_PER_HOUR;
+}
