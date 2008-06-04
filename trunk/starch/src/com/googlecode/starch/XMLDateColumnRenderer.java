8162ea87f706d093b66aaac4bfac73d7e58d7ad7
carljmosca <carljmosca@a3080d55-7d4e-0410-8870-6f489ad99ab0>
2008-06-04 17:28:50 +0000

    fixed selected and alternate colors

--- trunk/starch/src/com/googlecode/starch/XMLDateColumnRenderer.java
+++ trunk/starch/src/com/googlecode/starch/XMLDateColumnRenderer.java
@@ -2,13 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.googlecode.starch;
 
 import com.googlecode.starch.util.DateConverter;
-import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Font;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -27,49 +28,29 @@ public class XMLDateColumnRenderer implements TableCellRenderer {
         // 'value' is value contained in the cell located at
         // (rowIndex, vColIndex)
 
-        if (isSelected) {
-            // cell (and perhaps other cells) are selected
-        }
-
         if (hasFocus) {
             // this cell is the anchor and the table has the focus
-        }        
-
+   
+        }
         // Configure the component with the specified value
         if (value instanceof XMLGregorianCalendar) {
-            label.setText(DateConverter.getDateAsString((XMLGregorianCalendar)value));
+            label.setText(DateConverter.getDateAsString((XMLGregorianCalendar) value));
+            label.invalidate();
         }
-        if ((rowIndex % 2) == 0) {
-            label.setBackground(firstBackgroundColor);
+        panel.setLayout(new BorderLayout());
+        panel.add(label, BorderLayout.CENTER);
+        if (isSelected) {
+            panel.setBackground(table.getSelectionBackground());
+            label.setForeground(table.getSelectionForeground());
         } else {
-            label.setBackground(secondBackgroundColor);
+            label.setForeground(table.getForeground());
         }
-
         // Set tool tip if desired
         //label.setToolTipText((String) value);
-
         // Since the renderer is a component, return itself
-        return label;
+        return panel;
     }
 
-    public Color getFirstBackgroundColor() {
-        return firstBackgroundColor;
-    }
-
-    public void setFirstBackgroundColor(Color firstBackgroundColor) {
-        this.firstBackgroundColor = firstBackgroundColor;
-    }
-
-    public Color getSecondBackgroundColor() {
-        return secondBackgroundColor;
-    }
-
-    public void setSecondBackgroundColor(Color secondBackgroundColor) {
-        this.secondBackgroundColor = secondBackgroundColor;
-    }
-    
-    private Color firstBackgroundColor;
-    private Color secondBackgroundColor;
     private JLabel label = new JLabel();
-
+    private JPanel panel = new JPanel();
 }
