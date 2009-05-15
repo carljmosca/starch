108dcf5bc98e3f1f8be5e56df1f6b9725e6ee8aa
carljmosca <carljmosca@a3080d55-7d4e-0410-8870-6f489ad99ab0>
2009-05-15 16:41:22 +0000

    added support for XMLGregorianCalendar

--- trunk/starch/src/main/java/com/googlecode/starch/TextConverter.java
+++ trunk/starch/src/main/java/com/googlecode/starch/TextConverter.java
@@ -4,6 +4,8 @@
  */
 package com.googlecode.starch;
 
+import java.text.SimpleDateFormat;
+import javax.xml.datatype.XMLGregorianCalendar;
 import org.jdesktop.beansbinding.Converter;
 
 /**
@@ -13,20 +15,25 @@ import org.jdesktop.beansbinding.Converter;
 public class TextConverter extends Converter {
 
     public TextConverter() {
+        sdf = new SimpleDateFormat(dateFormat);
     }
 
     @Override
     public Object convertForward(Object value) {
+
         if (value == null) {
             return "";
         }
         if (value instanceof String) {
             return getConvertedString((String) value);
+        } else if (value instanceof XMLGregorianCalendar) {
+            return sdf.format(((XMLGregorianCalendar) value).toGregorianCalendar().getTime());
         }
         return value;
     }
 
     public Object convertReverse(Object value) {
+
         if (value == null) {
             return "";
         }
@@ -61,6 +68,17 @@ public class TextConverter extends Converter {
     public void setToUpper(boolean toUpper) {
         this.toUpper = toUpper;
     }
+
+    public String getDateFormat() {
+        return dateFormat;
+    }
+
+    public void setDateFormat(String dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+    
     private boolean toUpper = false;
     private boolean toLower = false;
+    private SimpleDateFormat sdf;
+    private String dateFormat = "MM/dd/yyyy";
 }
