34b76757948be25756e661d7b2e927d080cc7d15
carljmosca <carljmosca@a3080d55-7d4e-0410-8870-6f489ad99ab0>
2009-06-13 17:29:37 +0000

    initial commit

---
+++ trunk/starch/src/main/java/com/googlecode/starch/ui/WantsValidationStatus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.starch.ui;
+
+/**
+ *
+ * @author moscac
+ */
+public interface WantsValidationStatus {
+
+    void validateFailed();
+    void validatePassed();
+
+}
